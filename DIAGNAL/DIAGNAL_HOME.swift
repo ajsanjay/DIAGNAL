3d9e87cdcb83a6fc99a9c92caadfed0ee8312aab
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-06 16:46:50 +0530

    Home page UI Done

--- DIAGNAL/DIAGNAL_HOME.swift
+++ DIAGNAL/DIAGNAL_HOME.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct DIAGNAL_HOME: View {
+    
+    @State private var isDisplayingMovies = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("Welcome User")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Button(action: {
+                    
+                }, label: {
+                    CartButton(titele: "Romantic Comedy")
+                })
+                .padding()
+            }
+            .navigationTitle("DIAGNAL")
+            .fullScreenCover(isPresented: $isDisplayingMovies, content: {
+//                FullScreenView(isPresented: $isPresentingFullScreen)
+            })
+        }
     }
 }
 
 #Preview {
     DIAGNAL_HOME()
+}
+
+struct CartButton: View {
+    let titele: LocalizedStringKey
+    var body: some View {
+        Text(titele)
+            .font(.title3)
+            .fontWeight(.semibold)
+            .frame(width: 260, height: 50)
+            .foregroundColor(.white)
+            .background(.green)
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    CartButton(titele: "Add to Cart")
 }
